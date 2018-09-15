e9f6a9c5fc7c33fe7dfc67e391da6230a6070bc5
Igor Rybak <jrybak2312@gmail.com>
2018-09-15 16:05:16 +0300

    Refactor "difference" operation logic

---
+++ siderian-core/src/main/java/com/github/jrybak2312/siderian/DefaultComparableTemporalDiscreteDomain.java
@@ -0,0 +1,49 @@
+package com.github.jrybak2312.siderian;
+
+import com.google.common.collect.DiscreteDomain;
+
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalUnit;
+
+/**
+ * @author Igor Rybak
+ * @since 09-May-2018
+ */
+class DefaultComparableTemporalDiscreteDomain<T extends Comparable<?> & Temporal> extends DiscreteDomain<T> {
+    private final static DefaultComparableTemporalDiscreteDomain INSTANCE = new DefaultComparableTemporalDiscreteDomain();
+
+    private DefaultComparableTemporalDiscreteDomain() {
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends Comparable<?> & Temporal> DefaultComparableTemporalDiscreteDomain<T> instance() {
+        return (DefaultComparableTemporalDiscreteDomain<T>) INSTANCE;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T next(T value) {
+        return (T) value.plus(1, getUnit(value));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T previous(T value) {
+        return (T) value.minus(1, getUnit(value));
+    }
+
+    @Override
+    public long distance(T start, T end) {
+        return getUnit(start).between(start, end);
+    }
+
+    private TemporalUnit getUnit(T value) {
+        return TemporalUnitSupport.getPrecision(value)
+                .orElseThrow(() -> new UnsupportedOperationException(getMessage(value.getClass())));
+    }
+
+    private String getMessage(Class<?> type) {
+        return "No default temporal unit for " + type.getName() + ". " +
+                "Use Interval.difference(Interval<T>, java.time.temporal.TemporalUnit) to provide temporal unit.";
+    }
+}
